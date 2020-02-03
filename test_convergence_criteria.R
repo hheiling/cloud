bee7b843a396e05e12e12e714e162e91e0c78741
hheiling <hheiling@live.unc.edu>
2020-02-03 15:50:06 -0500

    Udated code for simulation to examine output from latest version of fit_dat: new convergence criteria, random_walk implemented throughout algorithm. Output list of info needed for diagnostics.

--- test_convergence_criteria.R
+++ test_convergence_criteria.R
@@ -2,18 +2,19 @@
 # Also use adaptive random walk in Metropolis-within-Gibbs during E steps in fit_dat
 
 library(glmmPen)
-library(parallel)
-library(foreach)
-library(doParallel)
 library(stringr)
+
+source("test_fit_dat.R")
 
 N = 500
 K = 5
-seeds = 1:7
-sd_options = 1.0
+seeds = 101:125
+sd_options = c(0.5, 1.0)
 
-M = 10^4
-M_str = c("10^4")
+# seeds = 1:2
+# sd_options = c(0.5, 1.0)
+
+M = 5000
 
 s_l = length(seeds)
 sd_l = length(sd_options)
@@ -21,49 +22,67 @@ sd_l = length(sd_options)
 data_sim = list()
 
 for(sd_ in 1:length(sd_options)){
-  for(s in seeds){
+  for(s in 1:length(seeds)){
     dat =
       sim.data2(n = N, ptot = 2, pnonzero = 2, nstudies = K,
                 sd_raneff = sd_options[sd_], family = 'binomial',
                 slopes = T,
-                seed = s, imbalance = 1, pnonzerovar = 0, beta = c(0, 1, 1))
-    data_sim[[s+(sd_-1)*s_l]] = c(dat, seed = s)
+                seed = seeds[s], imbalance = 1, pnonzerovar = 0, beta = c(0, 2, 2))
+    
+    data_sim[[s+(sd_-1)*s_l]] = c(dat, seed = seeds[s], sd_ranef = sd_options[sd_])
   }
 }
 
-fit_sim = function(dat, M){
+# for(i in 1:length(data_sim)){
+#   print(data_sim[[i]]$seed)
+#   print(data_sim[[i]]$sd_ranef)
+# } 
+
+fit_sim = function(dat, M, adapt_RW_options = adaptControl(batch_length = 100, 
+                                                           burnin_batchnum = 1000,
+                                                           offset = 8500)){
   
   set.seed(dat$seed)
   # Set gibbs = T
-  fit_glmmPen = fit_dat(dat, lambda0 = 0, lambda1 = 0, conv = 0.001, nMC = 100, 
+  fit_glmmPen = fit_dat_test(dat, lambda0 = 0, lambda1 = 0, conv = 0.001, nMC = 100, 
                         family = "binomial", trace = 0, penalty = "grMCP",
-                        alpha = 1, nMC_max = 5000, t = 7,
-                        returnMC = T, ufull = NULL, coeffull = NULL, gibbs = T, maxitEM = 100, 
-                        ufullinit = NULL, 
-                        adapt_RW_options = adaptControl(batch_length = 100,
-                                                        burnin_batchnum = 500,
-                                                        offset = 9000)) 
+                        alpha = 1, nMC_max = 5000, t = 5,
+                        returnMC = T, ufull = NULL, coeffull = NULL, gibbs = T, maxitEM = 75, 
+                        ufullinit = NULL, MwG_sampler = "random_walk",
+                        adapt_RW_options = adapt_RW_options) 
   
-  # Log-likelihood - Pajor Method
+  coef = fit_glmmPen$coef
+  sigma = fit_glmmPen$sigma
+  u = fit_glmmPen$u
+  # gibbs_accept_rate = fit_glmmPen$gibbs_accept_rate
+  proposal_SD = fit_glmmPen$proposal_SD
+  
+  coef_record_all = fit_glmmPen$coef_record_all
+  rownames(coef_record_all) = 1:nrow(coef_record_all)
+  used_iter = apply(coef_record_all, 1, function(x) !any(is.na(x)))
+  coef_record = coef_record_all[used_iter,]
+  coef_record_tail = coef_record[(nrow(coef_record)-9):nrow(coef_record),]
+  
+  fit0_record = fit_glmmPen$fit0_record
+  fit0_intercepts = fit0_record[used_iter,1]
+  
+  Znew2 = fit_glmmPen$extra$Znew2
+  okindex = fit_glmmPen$extra$ok_index
+  J = fit_glmmPen$J
+  covgroup = fit_glmmPen$covgroup
+  
   # Posterior draws taken using Adaptive Random Walk with Random Scan
-  post_list = sample_mc_adapt(fit = fit_glmmPen$fit, cov = fit_glmmPen$sigma, y = dat$y, X = dat$X,
-                              Z = fit_glmmPen$extra$Znew2, nMC = M, family = "binomial",
-                              group = dat$group, d = nlevels(dat$group), okindex = fit_glmmPen$extra$ok_index,
-                              nZ = fit_glmmPen$extra$Znew2, gibbs = T, uold = fit_glmmPen$u, trace = 2,
-                              proposal_SD = matrix(1.0, nrow = K, ncol = q), batch = 0.0,
-                              batch_length = batch_length, offset = offset, burnin = burnin)
+  post_list = sample_mc_adapt(fit = fit_glmmPen$fit, cov = sigma, y = dat$y, X = dat$X,
+                              Z = Znew2, nMC = M, family = "binomial",
+                              group = dat$group, d = nlevels(dat$group), okindex = okindex,
+                              nZ = Znew2, gibbs = T, uold = u, trace = 2,
+                              proposal_SD = proposal_SD, batch = adapt_RW_options$burnin_batchnum,
+                              batch_length = adapt_RW_options$batch_length, 
+                              offset = adapt_RW_options$offset,
+                              burnin_batchnum = adapt_RW_options$burnin_batchnum)
   
   post_U = post_list$u0
   gibbs_accept_rate = post_list$gibbs_accept_rate
-  proposal_SD = post_list$proposal_SD
-  
-  ll_pajor = numeric(length(M))
-  
-  for(m in 1:length(M)){
-    ll_pajor[m] = CAME_IS(posterior = post_U, y = dat$y, X = dat$X, Z = dat$Z, 
-                          group = dat$group, coef = fit_glmmPen$coef, sigma = fit_glmmPen$sigma, 
-                          family = "binomial", M = M[m])
-  }
   
   # Log-likelihood - glmer
   data = data.frame(y = dat$y, dat$X[,-1], group = dat$group)
@@ -71,21 +90,28 @@ fit_sim = function(dat, M){
   
   fit_glmer = glmer(formula = y ~ X1 + X2 + (X1 + X2 | group), data = data, family = "binomial")
   
-  ll_glmer = logLik(fit_glmer)
-  
-  ll_pdif = (ll_pajor - ll_glmer)/ll_glmer
-  
-  return(list(dat = dat, ll_pajor = ll_pajor, ll_glmer = ll_glmer, ll_pdif = ll_pdif, 
-              post_U = post_U, gibbs_accept_rate = gibbs_accept_rate, proposal_SD = proposal_SD,
+  return(list(dat = dat, coef = coef, sigma = sigma, coef_record_tail = coef_record_tail,
+              fit0_intercepts = fit0_intercepts, 
+              fit_extras = list(Znew2 = Znew2, covgroup = covgroup, J = J, okindex = okindex),
+              post_U = post_U, 
+              gibbs_accept_rate = gibbs_accept_rate, proposal_SD = proposal_SD,
               fit_glmer = fit_glmer)) 
 }
 
 
-output =
-  foreach(i = 1:length(data_sim)) %do% {
-    library(glmmPen)
-    fit_sim(data_sim[[i]], M = M)
-  }
+output = list()
 
-save(output, file = "sim_conv_critera_output.RData")
+# for(i in 1:length(data_sim)){
+#   print("Start dataset i")
+#   output[[i]] = fit_sim(dat = data_sim[[i]], M = M)
+# }
+
+for(i in 1){
+  cat("Start dataset ", i, "\n")
+  output[[i]] = fit_sim(dat = data_sim[[i]], M = M)
+}
+
+sim_conv_crit = output
+
+save(sim_conv_crit, file = "sim_conv_critera_output.RData")
 
